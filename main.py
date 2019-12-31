da8905493ddfca3bce6cbca8df7df659acd81434
unknown <oricarmi92@gmail.com>
2019-12-31 10:55:37 +0200

    added spyder and main files

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 31 10:43:40 2019
+
+@author: Ori
+"""
+import numpy as np
+import scipy as sp
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
